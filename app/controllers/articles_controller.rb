408ceadaa0c49a778235261ab4833193722d09f9
kbjude <kbjude@gmail.com>
2021-07-19 21:05:58 +0300

    Add params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,27 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @rticle.save
+      redirect_to @article
+    else
+      render :new
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:tile, :body)
+  end
 end
