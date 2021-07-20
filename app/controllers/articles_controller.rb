f6694e4fa071e680fb09394fa9debcbdc27c476d
kbjude <kbjude@gmail.com>
2021-07-20 17:26:37 +0300

    Working controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,8 +27,17 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
 
-    if @article.update
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path
   end
 
   private
