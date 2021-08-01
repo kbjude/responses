ef259b94dc5976048e6e869f5f84d58e38a1ca42
kbjude <kbjude@gmail.com>
2021-08-02 00:32:28 +0300

    Add spec files

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  RSpec.describe 'User', type: :model do
+    describe 'Creating a user' do
+      it 'returns a newly created user' do
+          user = User.create(name: 'Jude', email: 'kbjude@gmail.com')
+          expect(user).to eql(user)
+      end
+      it 'returns a verified user at login' do
+          user = User.create(name: '')
+          expect(user.valid?).to eql(false)
+      end
+    end
+  end
 end
